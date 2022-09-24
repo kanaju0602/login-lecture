3aa23c57ab2f027d7c1ceb32345bce5434353bf4
kanaju0602 <kana0602@naver.com>
2022-09-24 15:21:16 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,16 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
 
-console.log("hello world")
-console.log("bye");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        psword : psword.value,
+    };
+}
+
